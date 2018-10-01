4472c15998d633410117c994a4be1aaf3bc08afd
Giulia Savorgnan <giulia.savorgnan@annalect.com>
2018-10-01 20:11:26 -0300

    first commit

--- lib/build.sh
+++ lib/build.sh
@@ -7,9 +7,10 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
 
 
 # Build NMS
-cd nms/src/cuda
+cd /kaggle/working/pytorch-retinanet/lib/nms/src/cuda
 echo "Compiling nms kernels by nvcc..."
 /usr/local/cuda/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
+echo "... done"
 cd ../../
 python build.py
 cd ../
